515420fc367ef19b66b6d224bfaf3bcdcecbc392
Matt Jamison <thirdshiftvideo@gmail.com>
2020-05-28 13:01:59 -0400

    comment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
+require 'rss'
+require 'open-uri'
+
+
+
 class WelcomeController < ApplicationController
   
-  
+
   def us_cert_rss
 
     require 'rss'
@@ -21,10 +26,12 @@ class WelcomeController < ApplicationController
   def index
     @adjective = ['Jaded', 'Jagged', 'Jangling', 'Jarring', 'Jaunty', 'Jazzy', 'Jeering', 'Jesting', 'Jiggish', 'Jimp', 'Jingoistic', 'Jinxed', 'Jittery', 'Jocose', 'Jocular', 'Jocund', 'Jovial', 'Judicious', 'Jural'].sample
   
-    # -----------
-    require 'rss'
-    require 'open-uri'
-    url = 'https://www.us-cert.gov/ncas/all.xml'
+    # ----------- 
+
+    # this would typically be in own object
+    
+    # url = 'https://www.us-cert.gov/ncas/all.xml'
+    url = 'all.xml'
     @titles = []
     open(url) do |rss|
       feed = RSS::Parser.parse(rss)
