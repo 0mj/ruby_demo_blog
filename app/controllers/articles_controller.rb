dbc1fc6c1e38275d4f618eec1eec585416ee4429
Matt Jamison <thirdshiftvideo@gmail.com>
2020-05-28 09:03:48 -0400

    dont forget about .inspect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
+    #    render plain: params[:article].inspect
         @article = Article.new(article_params)
         
         if @article.save
