e3cd8cb8e0c29827aafde3520e7415df8d70b3c2
Matt Jamison <thirdshiftvideo@gmail.com>
2020-05-28 10:18:22 -0400

    seed data with faker and rss returning to welcome view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,25 @@ class WelcomeController < ApplicationController
 
     require 'rss'
     require 'open-uri'
+    url = 'https://www.us-cert.gov/ncas/all.xml'
+    @titles = []
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      titles.push(feed.channel.title)
+      feed.items.each do |item|
+      
+        titles.push(item.title)
+      end
+    end
+    
+  end
 
+  def index
+    @adjective = ['Jaded', 'Jagged', 'Jangling', 'Jarring', 'Jaunty', 'Jazzy', 'Jeering', 'Jesting', 'Jiggish', 'Jimp', 'Jingoistic', 'Jinxed', 'Jittery', 'Jocose', 'Jocular', 'Jocund', 'Jovial', 'Judicious', 'Jural'].sample
+  
+    # -----------
+    require 'rss'
+    require 'open-uri'
     url = 'https://www.us-cert.gov/ncas/all.xml'
     @titles = []
     open(url) do |rss|
@@ -15,15 +33,13 @@ class WelcomeController < ApplicationController
       
         @titles.push(item.title)
       end
-      
-      
     end
-    @titles
+    # -----------
+
+
+
+
   end
 
-  def index
-    @adjective = ['Jaded', 'Jagged', 'Jangling', 'Jarring', 'Jaunty', 'Jazzy', 'Jeering', 'Jesting', 'Jiggish', 'Jimp', 'Jingoistic', 'Jinxed', 'Jittery', 'Jocose', 'Jocular', 'Jocund', 'Jovial', 'Judicious', 'Jural'].sample
-  end
 
-  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 100.times do 
+#     article_title = Faker::Book.title
+#     article_text = Faker::Movie.quote
+#     Article.create(title: article_title, text: article_text)
+# end
+
+
+
+
+50.times do 
+    article_title = Faker::Music.band
+    article_text = Faker::Music.album
+    Article.create(title: article_title, text: article_text)
+end
