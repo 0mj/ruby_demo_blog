e7eda12f5981d59ce5e9cf1befdcf262104cf8b3
Matt Jamison <thirdshiftvideo@gmail.com>
2020-05-28 09:04:25 -0400

    sample adjectives and us_cert_rss

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,29 @@
 class WelcomeController < ApplicationController
-  def index
+  
+  
+  def us_cert_rss
+
+    require 'rss'
+    require 'open-uri'
+
+    url = 'https://www.us-cert.gov/ncas/all.xml'
+    @titles = []
+    open(url) do |rss|
+      feed = RSS::Parser.parse(rss)
+      @titles.push(feed.channel.title)
+      feed.items.each do |item|
+      
+        @titles.push(item.title)
+      end
+      
+      
+    end
+    @titles
   end
+
+  def index
+    @adjective = ['Jaded', 'Jagged', 'Jangling', 'Jarring', 'Jaunty', 'Jazzy', 'Jeering', 'Jesting', 'Jiggish', 'Jimp', 'Jingoistic', 'Jinxed', 'Jittery', 'Jocose', 'Jocular', 'Jocund', 'Jovial', 'Judicious', 'Jural'].sample
+  end
+
+  
 end
