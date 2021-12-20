9d85bc8d96b3a673acad03da59dc4caeb853ebc2
Matt Jamison <thirdshiftvideo@gmail.com>
2021-12-20 14:26:48 -0500

    product files via rails scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'products/index'
   get 'welcome/index'
   # For details on the DSL ( (domain-specific language) )available within this file, see https://guides.rubyonrails.org/routing.html
+  # RMHL https://api.rubyonrails.org/v7.0.0/classes/ActionDispatch/Routing/Mapper/Resources.html#method-i-resources
   resources :articles do 
     resources :comments
   end
