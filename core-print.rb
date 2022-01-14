d35a977b53a22b96e4aed35bf48b640abb00d2f3
Matt Jamison <thirdshiftvideo@gmail.com>
2022-01-14 10:15:57 -0500

    produce a type error

---
+++ core-print.rb
@@ -0,0 +1,11 @@
+# The following will error. Giving you a
+# "TypeError no implicit conversion of Integer into String"
+print "2+3 is equal to " + 2 + 3 
+
+# One possible solution
+# print "2+3 is equal to "
+# print 2 + 3
+
+
+#or
+print "2+3 is equal to " + ( 2 + 3 ).to_s 
