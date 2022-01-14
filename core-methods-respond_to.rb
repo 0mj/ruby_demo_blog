120626a3502b95d16c6006418e04b23cce57f4cc
Matt Jamison <thirdshiftvideo@gmail.com>
2022-01-14 10:06:15 -0500

    puts comments in brain

--- core-methods-respond_to.rb
+++ core-methods-respond_to.rb
@@ -3,13 +3,12 @@
 .respond_to? takes a symbol and returns true if an object 
 can receive that method and false otherwise. For example,
 
-[1, 2, 3].respond_to?(:push)
-
-would return true, since you can call .push on an array object. However,
-
-[1, 2, 3].respond_to?(:to_sym)
-
-would return false, since you can’t turn an array into a symbol.
+ 
 =end 
+
+puts [1, 2, 3].respond_to?(:push) # true since you can call .push on an array object.
+puts [1, 2, 3].respond_to?(:to_sym) #false  since you can’t turn an array into a symbol.
+
+
 age = 26
-age.respond_to?(:next)
+puts age.respond_to?(:next) # true 
