1ee17fb9b671fa0b8a5f46b05eb8fa75c09004f1
Matt Jamison <thirdshiftvideo@gmail.com>
2022-01-26 12:21:51 -0500

    change to match commet description

--- looping-next-if.rb
+++ looping-next-if.rb
@@ -1,12 +1,13 @@
 =begin
 Next!
 
-The next keyword can be used to skip over certain steps in the loop. For instance, if we don’t want to print out the even numbers, we can write:
+The next keyword can be used to skip over certain steps in the loop. 
+For instance, if we don’t want to print out the even numbers, we can write:
 =end 
 i = 20
 loop do
   i -= 1
-  next if i % 2 != 0
+  next if i % 2 == 0
   puts "#{i}"
   break if i <= 0
 end
