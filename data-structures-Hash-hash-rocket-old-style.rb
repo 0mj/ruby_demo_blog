43d082300b4cc08c8b9375b64c1d86b577f1cec2
Matt Jamison <thirdshiftvideo@gmail.com>
2022-02-07 14:44:05 -0500

    hash old style example

---
+++ data-structures-Hash-hash-rocket-old-style.rb
@@ -0,0 +1,13 @@
+# Hash https://ruby-doc.org/core-3.1.0/Hash.html
+# A Hash maps each of its unique keys to a specific value.
+
+# A Hash has certain similarities to an Array, but:
+
+# An Array index is always an Integer.
+
+# A Hash key can be (almost) any object.
+
+# Hash Data Syntax
+# The older syntax for Hash data uses the "hash rocket" =>:
+
+h = {:foo => 0, :bar => 1, :baz => 2} # => {:foo=>0, :bar=>1, :baz=>2}
