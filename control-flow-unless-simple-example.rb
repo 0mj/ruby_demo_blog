808948df6fc84b13d1d0ff4b76b949c97834b506
Matt Jamison <thirdshiftvideo@gmail.com>
2022-02-07 10:24:42 -0500

    unless examples and link to ruby doc

--- control-flow-unless-simple-example.rb
+++ control-flow-unless-simple-example.rb
@@ -1,3 +1,4 @@
+# https://ruby-doc.org/core-3.0.3/doc/syntax/control_expressions_rdoc.html#label-unless+Expression
 # unless
 # The keyword unless is a conditional.
 # It checks if an expression is false.
--- control-flow-unless.rb
+++ control-flow-unless.rb
@@ -1,4 +1,5 @@
 # UNLESS
+# https://ruby-doc.org/core-3.0.3/doc/syntax/control_expressions_rdoc.html#label-unless+Expression
 print "true or false. Are you focused? "
 focused = gets.chomp
 focused == 'true' ? focused = true : focused = false
